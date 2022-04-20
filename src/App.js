9810c0d8c9317866ea16f91fba4bcdb58e7f9ba0
karan <karanbisht844@gmail.com>
2022-04-20 15:57:25 +0530

    commit all

--- src/App.js
+++ src/App.js
@@ -1,37 +1,67 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import TextForm from './components/TextForm';
+import React,{useState} from 'react';
+import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+   Switch,
+  Route
+  
+} from "react-router-dom";
 
-
-let name='karan bisht';
+let name = 'karan bisht';
 function App() {
-  return (
-<>
-<nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">TextUtils</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">About</a>
-        </li>
+  const [mode,setMode] = useState('light');
+  const [alert,setAlert]=useState(null);
 
-      </ul>
-      <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
- 
-</>
+  const showAlert=(message,type) =>{
+    setAlert({
+      msg:message,
+      type:type 
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  }
+  const toggleMode =() =>{
+    if(mode === 'light'){
+      setMode('dark')
+      document.body.style.backgroundColor ='grey';
+      showAlert('dark mode has been enabled','success'  ); 
+    document.title ='TextUtils -Dark Mode'
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor ='white'
+      showAlert('light mode has been enabled','success')
+      document.title ='TextUtils -Light Mode'
+    }
+  }
+  return (
+    <>
+    <Router>
+      <Navbar title='TextUtils2h' mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert}/>
+      <div className="container my-3">
+
+      <Switch>
+          <Route path="/about" >
+            <About/>
+          </Route>
+          <Route path="/">   
+        <TextForm showAlert={showAlert} heading='enter the text to analize' mode={mode}/>
+           
+          </Route>
+        </Switch>
+     
+     
+       
+      </div>
+      </Router>
+    </>
   );
 }
-
 export default App;
